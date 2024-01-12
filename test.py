56ae0b17be8bf57c331fc2fa98abd1bab3a0f1c1
wathika-eng <wathika02@gmail.com>
2024-01-12 22:51:39 +0300

    start

---
+++ test.py
@@ -0,0 +1,15 @@
+# import uuid
+
+# x = uuid.uuid1()
+
+# print(uuid.uuid3(x, "Jowie"))
+import datetime
+import json
+
+# d = datetime.date.today()
+# bday = datetime.date(2002, 9, 26)
+# till_day = d - bday
+# print(till_day.days)
+
+t = datetime.datetime.today()
+x = json.t 
